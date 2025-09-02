cdfc037394015695a181293e631289d1b8e74df6
youssef taoussi <yousseftaoussi2003@gmail.com>
2025-09-02 02:14:08 +0100

    .

--- main.go
+++ main.go
@@ -15,26 +15,9 @@ import (
 
 const port = 42069
 
-func printRequest(r *request.Request) {
-	fmt.Printf(
-		"%s %s %s\n",
-		r.RequestLine.Method,
-		r.RequestLine.RequestTarget,
-		r.RequestLine.HttpVersion)
-	for key, value := range r.Headers {
-		fmt.Printf("%s: %s\n", key, value)
-	}
-	fmt.Println()
-	fmt.Println(string(r.Body))
-	fmt.Println()
-}
-
 func handler(w *response.Writer, req *request.Request) {
 	w.WriteStatusLine(200)
 	newHeaders := headers.NewHeaders()
-	// newHeaders["Content-Type"] = "text/html"
-	fmt.Println("Method: ", req.RequestLine.Method)
-	fmt.Println("Method: ", req.RequestLine.RequestTarget)
 	newHeaders["Content-Type"] = "application/json"
 	data, err := os.ReadFile("./test.json")
 	if err != nil {
